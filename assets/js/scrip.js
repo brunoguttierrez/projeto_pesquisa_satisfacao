37deb01acbbd1b1e6fb690a75935dd3f2ad3aced
bruno.netto <brunoguttierrezfn@gmail.com>
2024-04-09 08:04:46 -0300

    Atualizando o arquivo js

--- assets/js/scrip.js
+++ assets/js/scrip.js
@@ -2,18 +2,35 @@ document.getElementById('message').addEventListener('click', function(event) {
     event.preventDefault(); // Evita o comportamento padrão do botão enviar (submit)
 
     // Oculta o formulário
-    document.getElementById('my_form').style.display = 'none';
+    var myForm = document.getElementById('my_form');
+    myForm.style.display = 'none';
+    myForm.style.opacity = '0';
+    myForm.style.visibility = 'hidden';
 
     // Exibe a mensagem de sucesso
     document.querySelector('.show_message').style.display = 'block';
+    sessionStorage.setItem('formHidden', 'true');
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Verificar se o valor indicando que o formulário deve permanecer oculto está presente no sessionStorage
+    var formHidden = sessionStorage.getItem('formHidden');
+
+    if (formHidden) {
+        // Manter o formulário oculto
+        document.getElementById('my_form').style.display = 'none';
+        document.querySelector('.show_message').style.display = 'block';
+    }
+});
+
 
 const opcoes = document.querySelectorAll('.option');
 
 opcoes.forEach(opcao => {
-    opcao.addEventListener('click', () => {
+    opcao.addEventListener('click', (event) => {
         event.preventDefault()
         opcoes.forEach(o => o.classList.remove('selecionado'));
         opcao.classList.add('selecionado');
     });
 });
+
